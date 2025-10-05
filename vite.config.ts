b67a292c63cb7d68591a87fe9a2b7ee1798fc7a7
FlyFayer-byte <krupa.mykhailo@icloud.com>
2025-10-05 19:02:06 +0200

    modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import { defineConfig } from 'vite';
 const isProd = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
-  base: isProd ? '/01-ts-basics/' : './',
+  base: '/01-ts-basics/', // шлях до репозиторію на GitHub Pages
 });
