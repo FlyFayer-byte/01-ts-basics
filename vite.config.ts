1dbc2dae7691c78897ed9066cd5990c705339872
FlyFayer-byte <krupa.mykhailo@icloud.com>
2025-10-26 20:53:54 +0100

    modified:   index.html 	modified:   vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-// vite.config.ts
 export default defineConfig({
-  base: '/01-ts-basics/',
+  plugins: [react()],
+  base: '/01-ts-basics/', // <<< ОБОВ’ЯЗКОВО для GitHub Pages
 })
